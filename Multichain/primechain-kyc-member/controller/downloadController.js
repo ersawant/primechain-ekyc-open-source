806649e762b789a7e49350043257c51fc144f72a
IAnilNakoti <anil@primechain.in>
2018-07-22 09:51:33 +0530

    Update downloadController.js

--- Multichain/primechain-kyc-member/controller/downloadController.js
+++ Multichain/primechain-kyc-member/controller/downloadController.js
@@ -1,45 +1,94 @@
 const dataStorage = require('../BlockchainLib/storage');
 const config = require('../configs/blockchain_config');
 const common_utility = require('../lib/common_utility');
+const formModel = require("../model/form");
 
 module.exports = {
     download_document: (req, res, next) => {
         try {
-            let result = common_utility.hex2json(req.body.key);
-            // Querying the data from the blockchain
-            dataStorage.getStreamItem(config["STREAMS"]["KYC_RECORD_STREAM"], req.body.document_txid, (err, data_info) => {
-                if (err) { return next(err); }
+            let document_txid = req.body.document_txid;
+            let publisher_address = req.body.publisher_address;
 
-                if (data_info) {
-                    // Retrieving the actual data from the blockchain by passing txid and vout.
-                    dataStorage.getTransactionData(data_info["txid"], data_info["vout"], (err, data_hex) => {
-                        if (err) { return next(err); }
+            if (publisher_address === req.user.user_address) {
+                formModel.getEncryptionKeyByDocumentTxid(document_txid, (err, result) => {
+                    if (err) { return next(err); }
 
-                        let data_json = common_utility.hex2json(data_hex);
-                        let decrypted_data = common_utility.decryptiv(data_json, result["password"], result["iv"]);
-                        let decrypted_data_json = common_utility.str2json(decrypted_data);
-                        let document_data = common_utility.hex2bin(decrypted_data_json["data"]);
+                    if (result) {
+                        // Querying the data from the blockchain
+                        dataStorage.getStreamItem(config["STREAMS"]["KYC_RECORD_STREAM"], document_txid, (err, data_info) => {
+                            if (err) { return next(err); }
 
-                        res.writeHead(200, {
-                            'Content-Type': decrypted_data_json["mimetype"],
-                            'Content-disposition': 'attachment;filename=' + decrypted_data_json["name"],
-                            'Content-Length': document_data.length
+                            if (data_info) {
+                                // Retrieving the actual data from the blockchain by passing txid and vout.
+                                dataStorage.getTransactionData(data_info["txid"], data_info["vout"], (err, data_hex) => {
+                                    if (err) { return next(err); }
+
+                                    let data_json = common_utility.hex2json(data_hex);
+                                    let decrypted_data = common_utility.decryptiv(data_json, result["password"], result["iv"]);
+                                    let decrypted_data_json = common_utility.str2json(decrypted_data);
+                                    let document_data = common_utility.hex2bin(decrypted_data_json["data"]);
+
+                                    res.writeHead(200, {
+                                        'Content-Type': decrypted_data_json["mimetype"],
+                                        'Content-disposition': 'attachment;filename=' + decrypted_data_json["name"],
+                                        'Content-Length': document_data.length
+                                    });
+                                    // push the file data in response
+                                    res.end(document_data);
+
+                                });
+                            }
+                            else {
+                                // flash is used to display message in the view page.
+                                req.flash("error_msg", "You are not authorized to download this file");
+                                res.redirect(common_utility.refererPathExtractor(req));
+                            }
                         });
-                        // push the file data in response
-                        res.end(document_data);
+                    }
+                    else {
+                        // flash is used to display message in the view page.
+                        req.flash("error_msg", "Unable to fetch key");
+                        res.redirect(common_utility.refererPathExtractor(req));
+                    }
+                });
+            }
+            else {
+                let result = common_utility.hex2json(req.body.key);
+                // Querying the data from the blockchain
+                dataStorage.getStreamItem(config["STREAMS"]["KYC_RECORD_STREAM"], document_txid, (err, data_info) => {
+                    if (err) { return next(err); }
 
-                    });
-                }
-                else {
-                    // flash is used to display message in the view page.
-                    req.flash("error_msg", "You are not authorized to download this file");
-                    res.redirect(common_utility.refererPathExtractor(req));
-                }
-            });
+                    if (data_info) {
+                        // Retrieving the actual data from the blockchain by passing txid and vout.
+                        dataStorage.getTransactionData(data_info["txid"], data_info["vout"], (err, data_hex) => {
+                            if (err) { return next(err); }
+
+                            let data_json = common_utility.hex2json(data_hex);
+                            let decrypted_data = common_utility.decryptiv(data_json, result["password"], result["iv"]);
+                            let decrypted_data_json = common_utility.str2json(decrypted_data);
+                            let document_data = common_utility.hex2bin(decrypted_data_json["data"]);
+
+                            res.writeHead(200, {
+                                'Content-Type': decrypted_data_json["mimetype"],
+                                'Content-disposition': 'attachment;filename=' + decrypted_data_json["name"],
+                                'Content-Length': document_data.length
+                            });
+                            // push the file data in response
+                            res.end(document_data);
+
+                        });
+                    }
+                    else {
+                        // flash is used to display message in the view page.
+                        req.flash("error_msg", "You are not authorized to download this file");
+                        res.redirect(common_utility.refererPathExtractor(req));
+                    }
+                });
+            }
         }
         catch (error) {
             //  flash is used to display the message in the view page.
-            req.flash("error_msg", "You are not authorized to download this file");
+            req.flash("error_msg", error);
             res.redirect(common_utility.refererPathExtractor(req));
 
         }
